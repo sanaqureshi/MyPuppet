41ff53328e34bf40e429d69fd0b10d4df14706e1
sanaqureshi <qsanam8@gmail.com>
2017-07-05 18:54:31 +0530

    Update tomcat_install.pp

--- tomcat_install.pp
+++ tomcat_install.pp
@@ -1,10 +1,12 @@
 # 3.Write a script which do the installation of tomcat server and deploy a war file through it.
 # Installing Tomcat :
+# puppet module install puppetlabs/java
 
 class { "java":
   package => "java-1.8.0-openjdk-devel",
 }
 
+# puppet module install puppetlabs/tomcat
 tomcat::install { "/opt/tomcat":
   source_url => "https://archive.apache.org/dist/tomcat/tomcat-9/v9.0.0.M21/bin/apache-tomcat-9.0.0.M21.tar.gz",
 }
