09d4a79d903886a999c4d9d9dc92198b4c842ace
sanaqureshi <qsanam8@gmail.com>
2017-07-05 18:47:46 +0530

    Create tomcat_install.pp

---
+++ tomcat_install.pp
@@ -0,0 +1,23 @@
+# 3.Write a script which do the installation of tomcat server and deploy a war file through it.
+# Installing Tomcat :
+
+class { "java":
+  package => "java-1.8.0-openjdk-devel",
+}
+
+tomcat::install { "/opt/tomcat":
+  source_url => "https://archive.apache.org/dist/tomcat/tomcat-9/v9.0.0.M21/bin/apache-tomcat-9.0.0.M21.tar.gz",
+}
+tomcat::instance { "default":
+  catalina_home => "/opt/tomcat",
+}
+
+# Deploying war file :
+
+tomcat::war { "sample.war":
+  catalina_base => "/opt/tomcat",
+  war_source    => "/opt/tomcat/webapps/docs/appdev/sample/sample.war",
+}
+
+''' OUTPUT :
+'''
